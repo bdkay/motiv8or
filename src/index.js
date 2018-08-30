646f47b694e0e5d4701f6d4cfd9f2dad8b194505
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-30 01:25:22 -0500

    react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
 ReactDOM.render(
-  <div>App</div>, document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} />
+      <Route path="/App" component={App} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  </BrowserRouter>, document.getElementById('root')
 );
