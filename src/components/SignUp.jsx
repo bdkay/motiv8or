87b252fbb356e4324f9879f35f656cb59c4ef5a9
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-30 09:16:57 -0500

    basic auth

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 
 class SignUp extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+  
+  signUp(){
+    console.log('this.state', this.state);
+  }
+  
   render(){
     return (
-      <div className="form-inline">
+      <div className="form-inline col-lg-4">
       <h2>Sign Up</h2>
         <div className="form-group">
           <input
             className="form-control"
             type="text"
             placeholder="email"
+            onChange={(event) => this.setState({email: event.target.value})}
             />
           <input
             className="form-control"
             type="password"
             placeholder="password"
+            onChange={(event) => this.setState({password: event.target.value})}
           />
-          <button className="btn btn-primary" type="button">Sign Up</button>
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={() => this.signUp()}
+          >
+            Sign Up
+          </button>
         </div>
       </div>
     )
