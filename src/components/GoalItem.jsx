aa6442ef0cee3f251daf2ab64054751c8e931b1e
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-31 14:57:58 -0500

    this.props.goal

--- src/components/GoalItem.jsx
+++ src/components/GoalItem.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class GoalItem extends Component {
   render(){
+    console.log('this.props.goal', this.props.goal);
     return (
       <div>Goal Item</div>
     )
