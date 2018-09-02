bf16054796c4a38527c5123abd0c85406987643e
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-09-02 12:45:16 -0500

    beginning complete goal list

---
+++ src/components/CompleteGoalList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CompleteGoalList extends Component {
+  render (){
+    return(
+      <div>Complete Goal List</div>
+    )
+  }
+}
+
+export default CompleteGoalList;
